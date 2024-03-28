52b1ce366cf82db505794599c31a83b7061ed556
inkwje <dange@duck.com>
2024-03-28 13:05:02 +0100

    shell: indent with fish_indent
    idk why I didn't use it before

--- fish/config.fish
+++ fish/config.fish
@@ -3,20 +3,20 @@
 set -g fish_greeting
 
 if status is-login
-  set -gx XDG_CONFIG_HOME $HOME/.config/
-  set -gx XDG_CACHE_HOME $HOME/.cache/
-  set -gx XDG_DATA_HOME $HOME/.local/share
-  set -gx XDG_STATE_HOME $HOME/.local/state
+    set -gx XDG_CONFIG_HOME $HOME/.config/
+    set -gx XDG_CACHE_HOME $HOME/.cache/
+    set -gx XDG_DATA_HOME $HOME/.local/share
+    set -gx XDG_STATE_HOME $HOME/.local/state
 end
 
 if status is-interactive
-  fish_config theme choose "theme"
+    fish_config theme choose theme
 
-  set -gx EDITOR nvim
-  set -gx VISUAL nvim
+    set -gx EDITOR nvim
+    set -gx VISUAL nvim
 
-  set -gx GNUPGHOME $HOME/KEY/pgp/
-  set -gx GPG_TTY (tty)
+    set -gx GNUPGHOME $HOME/KEY/pgp/
+    set -gx GPG_TTY (tty)
 end
 
 abbr -a vi nvim
