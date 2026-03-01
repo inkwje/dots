a462ec51d7c1fcd041da077ba895b0358e0f57a0
inkwje <dange@duck.com>
2026-03-01 17:55:37 +0100

    zsh-autosuggestions
    how to bind unknown keystroke:
    bindkey '<CTRL+v><keystroke>'
    
    thanks to
    https://github.com/zsh-users/zsh-autosuggestions/issues/436#issuecomment-501567537

--- zsh/04_bindings.zsh
+++ zsh/04_bindings.zsh
@@ -2,7 +2,7 @@
 bindkey -d
 
 # https://github.com/zsh-users/zsh-autosuggestions?tab=readme-ov-file#key-bindings
-# bindkey '^]' autosuggest-accept     # CTRL+]: accepts the current suggestion
-# bindkey '^^[]' autosuggest-execute  # CTRL+ALT+]: accepts and executes the current suggestion
-# bindkey '^[c' autosuggest-clear     # ALT+C: clears the current suggestion
-# bindkey '^[t' autosuggest-toggle    # ALT+T: toggles between enabled/disabled suggestions
+bindkey '^]' autosuggest-accept     # CTRL+]: accepts the current suggestion
+bindkey '^[^]' autosuggest-execute  # ALT+CTRL+]: accepts and executes the current suggestion
+bindkey '^[c' autosuggest-clear     # ALT+c: clears the current suggestion
+bindkey '^[t' autosuggest-toggle    # ALT+t: toggles between enabled/disabled suggestions
