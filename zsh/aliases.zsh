68ca7d71e00acd6eb04a1aa238852a223068646f
inkwje <dange@duck.com>
2026-03-01 02:36:14 +0100

    shell: alias for zsh

---
+++ zsh/aliases.zsh
@@ -0,0 +1,8 @@
+# https://zsh.sourceforge.io/Doc/Release/Shell-Grammar.html#Aliasing
+
+# zsh
+alias zc="cd $ZDOTDIR"
+alias zr="source $ZDOTDIR/.zshrc"
+alias ze="$EDITOR $ZDOTDIR/.zshrc"
+alias zv="$EDITOR $ZDOTDIR/.zshenv"
+
